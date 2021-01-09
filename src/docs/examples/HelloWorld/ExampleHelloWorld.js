7c6be26e1dda5c73fca2dcbd1c660b4b51c67669
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-09 18:35:44 +0000

    Added first example

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import HelloWorld from '../../../components/HelloWorld/HelloWorld';
+
+const ExampleHelloWorld = () => (
+  <HelloWorld message=" everyone, thanks for visiting my site!" />
+);
+
+export default ExampleHelloWorld;
