c212c166d9d24d1ef7a7f95618e6573b624b71b5
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-09 17:25:44 +0000

    Copied generate file from web

--- scripts/generateComponentData.js
+++ scripts/generateComponentData.js
@@ -1,21 +1,55 @@
-import fs, { readFile, writeFile } from 'fs';
-import path from 'path';
-import chalk from 'chalk';
-import reactDocgen from 'react-docgen';
-const parse = reactDocgen.parse;
-import chokidar from 'chokidar';
+var fs = require('fs');
+var path = require('path');
+var chalk = require('chalk');
+var parse = require('react-docgen').parse;
+var chokidar = require('chokidar');
 
-const paths = {
+var paths = {
   examples: path.join(__dirname, '../src', 'docs', 'examples'),
   components: path.join(__dirname, '../src', 'components'),
   output: path.join(__dirname, '../config', 'componentData.js'),
 };
 
-const getComponentData = (paths, componentName) => {
-  const content = readFile(
-    path.join(paths.components, componentName, `${componentName}.js`)
+const enableWatchMode = process.argv.slice(2) == '--watch';
+if (enableWatchMode) {
+  // Regenerate component metadata when components or examples change.
+  chokidar
+    .watch([paths.examples, paths.components])
+    .on('change', function (event, path) {
+      generate(paths);
+    });
+} else {
+  // Generate component metadata
+  generate(paths);
+}
+
+function generate(paths) {
+  var errors = [];
+  var componentData = getDirectories(paths.components).map(function (
+    componentName
+  ) {
+    try {
+      return getComponentData(paths, componentName);
+    } catch (error) {
+      errors.push(
+        'An error occurred while attempting to generate metadata for ' +
+          componentName +
+          '. ' +
+          error
+      );
+    }
+  });
+  writeFile(
+    paths.output,
+    'module.exports = ' + JSON.stringify(errors.length ? errors : componentData)
   );
-  const info = parse(content);
+}
+
+function getComponentData(paths, componentName) {
+  var content = readFile(
+    path.join(paths.components, componentName, componentName + '.js')
+  );
+  var info = parse(content);
   return {
     name: componentName,
     description: info.description,
@@ -23,16 +57,14 @@ const getComponentData = (paths, componentName) => {
     code: content,
     examples: getExampleData(paths.examples, componentName),
   };
-};
+}
 
-const getExampleFiles = params => {};
-
-const getExampleData = (examplesPath, componentName) => {
-  const examples = getExampleFiles(examplesPath, componentName);
-  return examples.map(file => {
-    const filePath = path.join(examplesPath, componentName, file);
-    const content = readFile(filePath);
-    const info = parse(content);
+function getExampleData(examplesPath, componentName) {
+  var examples = getExampleFiles(examplesPath, componentName);
+  return examples.map(function (file) {
+    var filePath = path.join(examplesPath, componentName, file);
+    var content = readFile(filePath);
+    var info = parse(content);
     return {
       // By convention, component name should match the filename.
       // So remove the .js extension to get the component name.
@@ -41,29 +73,38 @@ const getExampleData = (examplesPath, componentName) => {
       code: content,
     };
   });
-};
+}
 
-const generate = paths => {
-  const errors = [];
-  const componentData = getDirectories(paths.components).map(componentName => {
-    try {
-      return getComponentData(paths, componentData);
-    } catch (error) {
-      errors.push(
-        `An error occurred while attempting to generate metadata for ${componentName}. ${error}`
-      );
-    }
+function getExampleFiles(examplesPath, componentName) {
+  var exampleFiles = [];
+  try {
+    exampleFiles = getFiles(path.join(examplesPath, componentName));
+  } catch (error) {
+    console.log(chalk.red(`No examples found for ${componentName}.`));
+  }
+  return exampleFiles;
+}
+
+function getDirectories(filepath) {
+  return fs.readdirSync(filepath).filter(function (file) {
+    return fs.statSync(path.join(filepath, file)).isDirectory();
   });
-  writeFile(
-    paths.output,
-    `module.exports = ${JSON.stringify(errors.length ? errors : componentData)}`
-  );
-};
+}
 
-const enableWatchMode = process.argv.slice(2) == '--watch';
-if (enableWatchMode) {
-  // Regenerate component metadata when components or examples change.
-  chokidar
-    .watch([paths.examples, paths.components])
-    .on('change', (event, path) => generate(paths));
+function getFiles(filepath) {
+  return fs.readdirSync(filepath).filter(function (file) {
+    return fs.statSync(path.join(filepath, file)).isFile();
+  });
+}
+
+function writeFile(filepath, content) {
+  fs.writeFile(filepath, content, function (err) {
+    err
+      ? console.log(chalk.red(err))
+      : console.log(chalk.green('Component data saved.'));
+  });
+}
+
+function readFile(filePath) {
+  return fs.readFileSync(filePath, 'utf-8');
 }
