9d75b5a474739531165383b19b0f86f3e4e97c3a
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-09 21:00:35 +0000

    Added code highlighting

--- src/docs/CodeExample.js
+++ src/docs/CodeExample.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-
 import Prism from 'prismjs';
+import './themes/prism-base16-ateliersulphurpool.light.css';
 
-const CodeExample = ({ children }) => (
-  <pre className="line-numbers">
-    <code className="language-javascript">{children}</code>
-  </pre>
-);
+const CodeExample = ({ children }) => {
+  useEffect(() => Prism.highlightAll(), []);
+
+  return (
+    <pre className="line-numbers">
+      <code className="language-javascript">{children}</code>
+    </pre>
+  );
+};
 
 CodeExample.propTypes = {
   children: PropTypes.string.isRequired,
