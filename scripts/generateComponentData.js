0de757c684aad5a59aab6819e686860805eb153d
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-09 17:02:14 +0000

    Added more to generator

--- scripts/generateComponentData.js
+++ scripts/generateComponentData.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs, { writeFile } from 'fs';
 import path from 'path';
 import chalk from 'chalk';
 import reactDocgen from 'react-docgen';
@@ -10,3 +10,28 @@ const paths = {
   components: path.join(__dirname, '../src', 'components'),
   output: path.join(__dirname, '../config', 'componentData.js'),
 };
+
+const generate = paths => {
+  const errors = [];
+  const componentData = getDirectories(paths.components).map(componentName => {
+    try {
+      return getComponentData(paths, componentData);
+    } catch (error) {
+      errors.push(
+        `An error occurred while attempting to generate metadata for ${componentName}. ${error}`
+      );
+    }
+  });
+  writeFile(
+    paths.output,
+    `module.exports = ${JSON.stringify(errors.length ? errors : componentData)}`
+  );
+};
+
+const enableWatchMode = process.argv.slice(2) == '--watch';
+if (enableWatchMode) {
+  // Regenerate component metadata when components or examples change.
+  chokidar
+    .watch([paths.examples, paths.components])
+    .on('change', (event, path) => generate(paths));
+}
