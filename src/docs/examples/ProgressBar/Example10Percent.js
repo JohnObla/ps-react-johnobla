c035043b4879e41151e44d9eabac76d1f2d56f39
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-09 22:29:57 +0000

    Added example

---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+const Example10Percent = () => {
+  return <ProgressBar percent={10} width={150} />;
+};
+
+export default Example10Percent;
