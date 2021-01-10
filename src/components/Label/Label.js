ec02481c33439395641a5d0c083a79a4be75df16
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-10 01:11:20 +0000

    Added space before label

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 /** Label with required field display, htmlFor and block styling */
 const Label = ({ htmlFor, label, required = false }) => (
   <label className="Label" htmlFor={htmlFor}>
-    {label} {required && <span className="Label__required">*</span>}
+    {label} {required && <span className="Label__required"> *</span>}
   </label>
 );
 export default Label;
