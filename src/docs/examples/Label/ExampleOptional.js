190bbe7dc51a60618139c6e40937fae247a101a7
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-10 01:18:15 +0000

    Added examples

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+/** Optional label */
+const ExampleOptional = () => {
+  return <Label htmlFor="address-line-2" label="Address Line 2" />;
+};
+
+export default ExampleOptional;
---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+/** Required label */
+const ExampleRequired = () => {
+  return <Label htmlFor="address-line-1" label="Address Line 1" required />;
+};
+
+export default ExampleRequired;
