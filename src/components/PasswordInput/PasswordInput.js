d0f539cf98968c0100138d96403e6c9e5d8021cf
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-11 06:32:34 +0000

    Removed unused useState

--- src/components/PasswordInput/PasswordInput.js
+++ src/components/PasswordInput/PasswordInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import useToggleBool from '../../hooks/useToggleBool';
 import ProgressBar from '../ProgressBar';
